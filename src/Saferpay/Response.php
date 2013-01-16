5364637ba09c6409820103d97aad08df038b05c9
Richard Kuhnt <r15ch13+git@gmail.com>
2013-01-16 22:57:50 +0100

    Add function to access response data

--- src/Saferpay/Response.php
+++ src/Saferpay/Response.php
@@ -7,6 +7,13 @@ abstract class Response
     private $signature = '';
     private $verify;
 
+    public function getData($attribute)
+    {
+        if(isset($this->data[$attribute]))
+            return $this->data[$attribute];
+        return null;
+    }
+
     // <IDP MSGTYPE="InsertCardResponse" KEYID="1-0" RESULT="7000" SCDRESULT="7000" SCDDESCRIPTION="An error occurred" DESCRIPTION="An error occurred" CARDREFID="" LIFETIME="" ACCOUNTID="99867-94913159" />
 
     public function __construct($data, $signature = '')
