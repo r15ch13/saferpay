79bcab0a08c01ea878ea0365f93060254834287f
Richard Kuhnt <r15ch13+git@gmail.com>
2013-02-01 14:09:13 +0100

    Fix tests

--- tests/Saferpay/RegisterCard_Test.php
+++ tests/Saferpay/RegisterCard_Test.php
@@ -8,20 +8,20 @@ class RegisterCardTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->registercard = \Saferpay\RegisterCard::create();
+        $this->registercard = \Saferpay\Request\RegisterCard::create();
     }
 
     public function testRegisterCardObjectCreated() {
-        $this->assertInstanceOf('\Saferpay\RegisterCard', $this->registercard);
+        $this->assertInstanceOf('\Saferpay\Request\RegisterCard', $this->registercard);
     }
 
     public function testResponsqweqweributesAreParsedCorrectly()
     {
-        $registercard = \Saferpay\RegisterCard::create()
-            ->successlink('http://saferpay.dev/test.php?success')
-            ->faillink('http://saferpay.dev/test.php?fail');
+        $this->registercard
+            ->setAttribute('successlink', 'http://saferpay.dev/test.php?success')
+            ->setAttribute('faillink', 'http://saferpay.dev/test.php?fail');
 
-        $result = $registercard->execute();
+        $result = $this->registercard->execute();
         $this->assertStringStartsWith('https://www.saferpay.com/vt2/Pay.aspx?DATA=', $result);
         $this->assertNotNull($result);
     }
